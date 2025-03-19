190d49a3c9f095abea0db0615bc8ca5274c7f789
lasse2507a <lran18@student.aau.dk>
2025-03-19 15:02:17 +0100

    cleanup

--- src/inference.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-from huggingface_hub import login
-
-login(token=("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"))
-
-text = "The film's storyline is well-structured, and the acting is competent, but it doesn't stand out in any remarkable way. Overall, it's neither exceptionally good nor particularly bad—it just is."
-classifier = pipeline("sentiment-analysis", model="Dolmer/my_awesome_model")
-result = classifier(text)
-print(result)
