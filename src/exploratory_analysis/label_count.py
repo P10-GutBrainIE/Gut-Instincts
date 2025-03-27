bba9e019a61a406c7524ce7aa25a60c77490d742
lasse2507a <lran18@student.aau.dk>
2025-03-27 13:14:26 +0100

    text length histogram + new colormap

--- src/exploratory_analysis/label_count.py
+++ src/exploratory_analysis/label_count.py
@@ -27,12 +27,12 @@ def label_distribution_stacked_bar_plot(label_distribution: dict):
 	x = np.arange(len(labels))
 	bottom = np.zeros(len(labels))
 
-	palette = sns.color_palette()
+	palette = sns.color_palette("magma", n_colors=4, desat=0.7)
 
 	plt.figure(figsize=(14, 7), dpi=300)
 	for i, quality in enumerate(qualities):
 		counts = df_pivot[quality].values
-		plt.bar(x, counts, bottom=bottom, color=palette[3 - i], edgecolor="grey", linewidth=0.5, label=quality)
+		plt.bar(x, counts, bottom=bottom, color=palette[i], edgecolor="black", linewidth=0.5, label=quality)
 		bottom += counts
 
 	for idx, total in enumerate(totals):
--- src/exploratory_analysis/text_length.py
+++ src/exploratory_analysis/text_length.py
@@ -2,11 +2,104 @@ import json
 import os
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
+import numpy as np
 import seaborn as sns
 import pandas as pd
 
 
-def analyze_text_length_distribution(file_paths):
+def text_length_histogram(file_paths: str, save_path: str = os.path.join("plots", "text_length_histogram.png")):
+	text_lengths = {quality: {"title": [], "abstract": []} for quality in file_paths.keys()}
+
+	for quality, file_path in file_paths.items():
+		with open(file_path, "r", encoding="utf-8") as f:
+			file_data = json.load(f)
+
+			for _, content in file_data.items():
+				text_lengths[quality]["title"].append(len(content["metadata"]["title"].split(" ")))
+				text_lengths[quality]["abstract"].append(len(content["metadata"]["abstract"].split(" ")))
+
+	data = []
+	for quality, lengths in text_lengths.items():
+		for text_type in ["title", "abstract"]:
+			for length in lengths[text_type]:
+				data.append({"quality": quality, "text type": text_type, "length": length})
+	df = pd.DataFrame(data)
+
+	df = df[df["length"] <= 900]
+
+	sns.set_theme(style="ticks")
+	fig, axes = plt.subplots(2, 4, figsize=(14, 7))
+
+	palette = sns.color_palette("magma", n_colors=4, desat=0.7)[::-1]
+
+	qualities = ["platinum", "gold", "silver", "bronze"]
+
+	title_data = df[df["text type"] == "title"]["length"]
+	abstract_data = df[df["text type"] == "abstract"]["length"]
+
+	title_xlim = (title_data.min(), title_data.max())
+	abstract_xlim = (abstract_data.min(), abstract_data.max())
+
+	title_data = df[df["text type"] == "title"]["length"]
+	for i, quality in enumerate(qualities):
+		sns.histplot(
+			data=df[(df["text type"] == "title") & (df["quality"] == quality)],
+			x="length",
+			hue="quality",
+			palette=palette[i : i + 1],
+			multiple="stack",
+			ax=axes[0, i],
+			bins=25,
+			legend=False,
+			alpha=1,
+			edgecolor="black",
+			linewidth=0.5,
+		)
+		axes[0, i].set_xlabel("")
+		axes[0, i].set_ylabel("")
+		axes[0, i].set_xlim(title_xlim)
+		axes[0, i].set_xticks(np.arange(0, title_data.max(), step=10))
+	fig.text(0.47, 0.985, "Titles", ha="center", va="center", fontsize=16)
+
+	unique_qualities = df["quality"].unique()
+	handles = [Patch(color=palette[i], label=quality.capitalize()) for i, quality in enumerate(unique_qualities)]
+	handles.reverse()
+	axes[0, 3].legend(
+		handles=handles, title="Quality", bbox_to_anchor=(1, 1), loc="upper left", fontsize=12, title_fontsize=14
+	)
+
+	title_data = df[df["text type"] == "abstract"]["length"]
+	for i, quality in enumerate(qualities):
+		sns.histplot(
+			data=df[(df["text type"] == "abstract") & (df["quality"] == quality)],
+			x="length",
+			hue="quality",
+			palette=palette[i : i + 1],
+			multiple="stack",
+			ax=axes[1, i],
+			bins=20,
+			legend=False,
+			alpha=1,
+			edgecolor="black",
+			linewidth=0.5,
+		)
+		axes[1, i].set_xlabel("")
+		axes[1, i].set_ylabel("")
+		axes[1, i].set_xlim(abstract_xlim)
+		axes[1, i].set_xticks(np.arange(0, title_data.max(), step=100))
+	plt.subplots_adjust(hspace=5)
+	fig.text(0.47, 0.485, "Abstracts", ha="center", va="center", fontsize=16)
+
+	plt.figtext(0.47, 0.0, "Word Count", ha="center", fontsize=14)
+	plt.figtext(0.006, 0.47, "Frequency", ha="center", rotation=90, fontsize=14)
+
+	sns.despine()
+	plt.tight_layout(pad=1.3)
+	os.makedirs("plots", exist_ok=True)
+	plt.savefig(save_path, dpi=300)
+
+
+def text_length_violinplot(file_paths: str, save_path: str = os.path.join("plots", "text_length_violinplot.png")):
 	text_lengths = {quality: {"title": [], "abstract": []} for quality in file_paths.keys()}
 
 	for quality, file_path in file_paths.items():
@@ -29,7 +122,7 @@ def analyze_text_length_distribution(file_paths):
 	sns.set_theme(style="ticks")
 	_, axes = plt.subplots(2, 1, figsize=(14, 7))
 
-	palette = sns.color_palette()
+	palette = sns.color_palette("magma", n_colors=4, desat=0.7)[::-1]
 
 	sns.violinplot(
 		x="quality",
@@ -70,7 +163,7 @@ def analyze_text_length_distribution(file_paths):
 	plt.tight_layout()
 
 	os.makedirs("plots", exist_ok=True)
-	plt.savefig(os.path.join("plots", "text_lengths.png"), dpi=300)
+	plt.savefig(save_path, dpi=300)
 
 
 if __name__ == "__main__":
@@ -82,4 +175,5 @@ if __name__ == "__main__":
 		"bronze": os.path.join(shared_path, "bronze_quality", "json_format", "train_bronze.json"),
 	}
 
-	analyze_text_length_distribution(file_paths)
+	text_length_histogram(file_paths)
+	text_length_violinplot(file_paths)
