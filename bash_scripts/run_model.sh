6b080ed4cef06a8519574783bb16ee18595e22a8
Marius <marius@gardshodn.com>
2025-04-10 10:04:35 +0200

    new training_configs

--- bash_scripts/run_model.sh
+++ bash_scripts/run_model.sh
@@ -9,15 +9,16 @@
 mkdir -p logs
 
 # ---------- BioLinkBERT-large ----------
-srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/BioLinkBert-large.yaml > logs/biolink.out 2> logs/biolink.err
+srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/BioLinkBert-large.yaml > logs/biolink-large.out 2> logs/biolink-large.err
 
 # ---------- BioLinkBERT-base ----------
+srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/BioLinkBert-base.yaml > logs/biolink-base.out 2> logs/biolink-base.err
 
+# ---------- BiomedNLP-BiomedBERT-base-uncased-abstract-fulltext ----------
+srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/BiomedBert-base-uncased-abstract-fulltext.yaml > logs/BiomedBERT-af.out 2> logs/BiomedBERT-af.err
 
-# ---------- BioMedBERT ----------
-
-
-# ---------- BiomedNLP-BiomedBERT-large-uncased-abstract ----------
-
+# ---------- BiomedNLP-BiomedBERT-base-uncased-abstract ----------
+srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/BiomedBert-base-uncased-abstract.yaml > logs/BiomedBERT-a.out 2> logs/BiomedBERT-a.err
 
 # ---------- BiomedELECTRA-base-uncased-abstract ----------
+srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/BiomedElectra-base-uncased-abstract.yaml > logs/BiomedELECTRA.out 2> logs/BiomedELECTRA.err
