d98a7c68066e0248c7a2fe480914604fbcb64ba4
Marius <marius@gardshodn.com>
2025-04-10 09:42:43 +0200

    new bash script

--- bash_scripts/test_script.sh
+++ bash_scripts/test_script.sh
@@ -8,16 +8,9 @@
 
 mkdir -p logs
 
-# ---------- BioLinkBERT-large ----------
+# ---------- BioLinkBERT ----------
 srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/BioLinkBert-large.yaml > logs/biolink.out 2> logs/biolink.err
 
-# ---------- BioLinkBERT-base ----------
 
 
-# ---------- BioMedBERT ----------
-
-
-# ---------- BiomedNLP-BiomedBERT-large-uncased-abstract ----------
-
-
-# ---------- BiomedELECTRA-base-uncased-abstract ----------
+#srun singularity exec --nv --bind ~/test_env:/test_env /ceph/container/python/python_3>  /bin/bash -c "source /test_env/bin/activate && python src/NER/training.py --model_na>  > logs/biolink.out 2> logs/biolink.err
