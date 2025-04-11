ec677017e8b8f66e2e4387cece2a2da4a77f38b9
Marius <marius@gardshodn.com>
2025-04-11 15:55:53 +0200

    bash changes

--- bash_scripts/run_model_x_amount_freeze-32-epochs.sh
+++ bash_scripts/run_model_x_amount_freeze-32-epochs.sh
@@ -10,13 +10,13 @@ mkdir -p logs
 
 # ---------- BioLinkBERT-large freeze 16/32 epochs ----------
 # preprocessing
-srun singularity exec /ceph/container/python/python_3.12.sif python src/preprocessing/main.py --config training_configs/BioLinkBert-large-freeze-16-epochs-out-of-32-epochs.yaml > logs/biolink-large-freeze-16-epochs-out-of-32-epochs.out 2> logs/biolink-large-freeze-16-epochs-out-of-32-epochs.err
+srun singularity exec /ceph/container/python/python_3.12.sif python src/preprocessing/main.py --config training_configs/BioLinkBert-large-freeze-16-epochs-out-of-32-epochs.yaml > logs/biolink-large-freeze-16-epochs-out-of-32-epochs-pre.out 2> logs/biolink-large-freeze-16-epochs-out-of-32-epochs-pre.err
 
 # training
-srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/BioLinkBert-large-freeze-16-epochs-out-of-32-epochs.yaml > logs/biolink-large-freeze-16-epochs-out-of-32-epochs.out 2> logs/biolink-large-freeze-16-epochs-out-of-32-epochs.err
+srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/BioLinkBert-large-freeze-16-epochs-out-of-32-epochs.yaml > logs/biolink-large-freeze-16-epochs-out-of-32-epochs-train.out 2> logs/biolink-large-freeze-16-epochs-out-of-32-epochs-train.err
 
 # inference
-srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/inference.py --config training_configs/BioLinkBert-large-freeze-16-epochs-out-of-32-epochs.yaml > logs/biolink-large-freeze-16-epochs-out-of-32-epochs.out 2> logs/biolink-large-freeze-16-epochs-out-of-32-epochs.err
+srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/inference.py --config training_configs/BioLinkBert-large-freeze-16-epochs-out-of-32-epochs.yaml > logs/biolink-large-freeze-16-epochs-out-of-32-epochs-inf.out 2> logs/biolink-large-freeze-16-epochs-out-of-32-epochs-inf.err
 
 # evaluation
 #srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/evaluation/evaluation.py --config training_configs/BioLinkBert-large-freeze-16-epochs-out-of-32-epochs.yaml > logs/biolink-large-freeze-16-epochs-out-of-32-epochs.out 2> logs/biolink-large-freeze-16-epochs-out-of-32-epochs.err
@@ -27,10 +27,10 @@ srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER
 #srun singularity exec /ceph/container/python/python_3.12.sif python src/preprocessing/main.py --config training_configs/BioLinkBert-large-freeze-16-epochs-out-of-32-epochs.yaml > logs/biolink-large-freeze-16-epochs-out-of-32-epochs.out 2> logs/biolink-large-freeze-16-epochs-out-of-32-epochs.err
 
 # training
-srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/BioLinkBert-large-freeze-20-epochs-out-of-32-epochs.yaml > logs/biolink-large-freeze-20-epochs-out-of-32-epochs.out 2> logs/biolink-large-freeze-20-epochs-out-of-32-epochs.err
+srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/BioLinkBert-large-freeze-20-epochs-out-of-32-epochs.yaml > logs/biolink-large-freeze-20-epochs-out-of-32-epochs-train.out 2> logs/biolink-large-freeze-20-epochs-out-of-32-epochs-train.err
 
 # inference
-srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/inference.py --config training_configs/BioLinkBert-large-freeze-20-epochs-out-of-32-epochs.yaml > logs/biolink-large-freeze-20-epochs-out-of-32-epochs.out 2> logs/biolink-large-freeze-20-epochs-out-of-32-epochs.err
+srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/inference.py --config training_configs/BioLinkBert-large-freeze-20-epochs-out-of-32-epochs.yaml > logs/biolink-large-freeze-20-epochs-out-of-32-epochs-inf.out 2> logs/biolink-large-freeze-20-epochs-out-of-32-epochs-inf.err
 
 # evaluation
 #srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/evaluation/evaluation.py --config training_configs/BioLinkBert-large-freeze-20-epochs-out-of-32-epochs.yaml > logs/biolink-large-freeze-20-epochs-out-of-32-epochs.out 2> logs/biolink-large-freeze-20-epochs-out-of-32-epochs.err
