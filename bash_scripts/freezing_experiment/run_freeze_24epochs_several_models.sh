1176cbb488a730e882cb85173b7202013d41b660
Marius <marius@gardshodn.com>
2025-04-12 14:28:37 +0200

    bash

--- bash_scripts/freezing_experiment/run_freeze_24epochs_several_models.sh
+++ bash_scripts/freezing_experiment/run_freeze_24epochs_several_models.sh
@@ -34,18 +34,18 @@ mkdir -p logs
 # # evaluation
 # srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/evaluation/evaluation.py --config training_configs/freezing_experiment/BiomedBERT-base-uncased-abstract-freeze-24epochs.yaml > logs/BiomedBERT-base-uncased-abstract-freeze-24epochs-eval.out 2> logs/BiomedBERT-base-uncased-abstract-freeze-24epochs-eval.err
 
-# # ---------- BiomedBERT-large-uncased-abstract ----------
-# # preprocessing
-# srun singularity exec /ceph/container/python/python_3.12.sif python src/preprocessing/main.py --config training_configs/freezing_experiment/BiomedBERT-large-uncased-abstract-freeze-24epochs.yaml > logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-pre.out 2> logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-pre.err
+# ---------- BiomedBERT-large-uncased-abstract ----------
+# preprocessing
+srun singularity exec /ceph/container/python/python_3.12.sif python src/preprocessing/main.py --config training_configs/freezing_experiment/BiomedBERT-large-uncased-abstract-freeze-24epochs.yaml > logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-pre.out 2> logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-pre.err
 
-# # training
-# srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/freezing_experiment/BiomedBERT-large-uncased-abstract-freeze-24epochs.yaml > logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-train.out 2> logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-train.err
+# training
+srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/freezing_experiment/BiomedBERT-large-uncased-abstract-freeze-24epochs.yaml > logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-train.out 2> logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-train.err
 
-# # inference
-# srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/inference.py --config training_configs/freezing_experiment/BiomedBERT-large-uncased-abstract-freeze-24epochs.yaml > logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-inf.out 2> logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-inf.err
+# inference
+srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/inference.py --config training_configs/freezing_experiment/BiomedBERT-large-uncased-abstract-freeze-24epochs.yaml > logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-inf.out 2> logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-inf.err
 
-# # evaluation
-# srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/evaluation/evaluation.py --config training_configs/freezing_experiment/BiomedBERT-large-uncased-abstract-freeze-24epochs.yaml > logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-eval.out 2> logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-eval.err
+# evaluation
+srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/evaluation/evaluation.py --config training_configs/freezing_experiment/BiomedBERT-large-uncased-abstract-freeze-24epochs.yaml > logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-eval.out 2> logs/BiomedBERT-large-uncased-abstract-freeze-24epochs-eval.err
 
 # ---------- BiomedBERT-base-uncased-abstract-fulltext ----------
 # preprocessing
