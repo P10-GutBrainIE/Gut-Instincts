3c465c2c5c1d8b63082d9c431f56424799da96a5
Marius <marius@gardshodn.com>
2025-04-14 09:56:09 +0200

    ensemble inference

--- src/NER/ensemble_inference.py
+++ src/NER/ensemble_inference.py
@@ -2,6 +2,7 @@ import os
 import json
 import argparse
 import yaml
+from utils.utils import save_json_data
 from collections import Counter, defaultdict
 from NER.inference import NERInference
 
@@ -25,31 +26,31 @@ def load_model_predictions(config):
 
 def majority_vote(predictions):
 	entity_votes = defaultdict(list)
-	ensemble_results = defaultdict(list)
+
 	for model_predictions in predictions:
 		for paper_id, content in model_predictions.items():
 			for entity in content["entities"]:
 				key = (paper_id, entity["start_idx"], entity["end_idx"], entity["location"])
 				entity_votes[key].append((entity["label"], entity["text_span"]))
-				
+
+	ensemble_results = defaultdict(lambda: {"entities": []})
+
 	for (paper_id, start_idx, end_idx, location), votes in entity_votes.items():
 		labels, spans = zip(*votes)
-		majority_label, _ = Counter(labels).most_common(1)[0]
-		majority_span = Counter(spans).most_common(1)[0]
-		ensemble_results[paper_id].append({
-			"start_idx": start_idx,
-			"end_idx": end_idx,
-			"location": location,
-			"text_span": majority_span,
-			"label": majority_label,
-        })
+		majority_label = Counter(labels).most_common(1)[0][0]
+		majority_span = Counter(spans).most_common(1)[0][0]
+
+		ensemble_results[paper_id]["entities"].append(
+			{
+				"start_idx": start_idx,
+				"end_idx": end_idx,
+				"location": location,
+				"text_span": majority_span,
+				"label": majority_label,
+			}
+		)
 
 	return dict(ensemble_results)
-
-
-def save_ensemble_results(predictions, save_path):
-	with open(save_path, "w") as file:
-		json.dump(predictions, file)
 
 
 if __name__ == "__main__":
@@ -66,4 +67,11 @@ if __name__ == "__main__":
 	ensemble_predictions = majority_vote(predictions)
 
 	print("Majority vote completed.")
-	save_ensemble_results(predictions=ensemble_predictions, save_path=os.path.join("data_inference_results", f"{config['experiment_name']}.json"))
+	save_json_data(
+		data=ensemble_predictions,
+		output_path=os.path.join("data_inference_results", f"{config['experiment_name']}.json"),
+	)
+	# save_ensemble_results(
+	# 	predictions=ensemble_predictions,
+	# 	save_path=os.path.join("data_inference_results", f"{config['experiment_name']}.json"),
+	# )
