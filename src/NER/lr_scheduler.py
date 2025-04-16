2b4e1200772c59c34e2993f90232a91c174d1b35
lasse2507a <lran18@student.aau.dk>
2025-04-16 18:56:59 +0200

    lr scheduler

--- src/NER/lr_scheduler.py
+++ src/NER/lr_scheduler.py
@@ -25,7 +25,10 @@ def lr_scheduler(lr_scheduler_dict: dict, optimizer) -> torch.optim.lr_scheduler
 
 			epochs_post_warmup = epoch - (lr_scheduler_dict["custom_schedule"][-1][1] + 1)
 
-			return lr_scheduler_dict["gamma"] ** (epochs_post_warmup // lr_scheduler_dict["step_size"])
+			if epochs_post_warmup <= lr_scheduler_dict["step_size"]:
+				return lr_scheduler_dict["custom_schedule"][-1][2]
+			else:
+				return lr_scheduler_dict["gamma"] ** (epochs_post_warmup // lr_scheduler_dict["step_size"])
 
 		scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda epoch: custom_schedule(epoch))
 
