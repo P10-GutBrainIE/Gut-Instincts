893adf4380b10a6fe3ad4b963cd49bfb9d872b3e
lasse2507a <lran18@student.aau.dk>
2025-04-17 12:00:56 +0200

    dataset class to avoid warning

--- src/NER/dataset.py
+++ src/NER/dataset.py
@@ -11,9 +11,9 @@ class Dataset(torch.utils.data.Dataset):
 
 	def __getitem__(self, idx):
 		sample = self.data[idx]
-		sample["input_ids"] = torch.tensor(sample["input_ids"], dtype=torch.long)
-		sample["attention_mask"] = torch.tensor(sample["attention_mask"], dtype=torch.long)
-		sample["labels"] = torch.tensor(sample["labels"], dtype=torch.long)
+		sample["input_ids"] = torch.as_tensor(sample["input_ids"], dtype=torch.long)
+		sample["attention_mask"] = torch.as_tensor(sample["attention_mask"], dtype=torch.long)
+		sample["labels"] = torch.as_tensor(sample["labels"], dtype=torch.long)
 		if self.with_weights:
-			sample["weight"] = torch.tensor(sample["weight"], dtype=torch.float)
+			sample["weight"] = torch.as_tensor(sample["weight"], dtype=torch.float)
 		return sample
