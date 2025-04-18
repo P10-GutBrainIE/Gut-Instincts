6a6d79eaa02fc30a005e7b0bdd8b0649691a03c4
Mikkel Dolmer <mdolme20@student.aau.dk>
2025-04-18 09:32:38 +0200

    debuggin

--- src/NER/architectures/bert_linear_crf.py
+++ src/NER/architectures/bert_linear_crf.py
@@ -2,28 +2,30 @@ import torch.nn as nn
 from transformers import AutoModel
 from torchcrf import CRF
 
+
 class BertLinearCRF(nn.Module):
-    def __init__(self, model_name: str, num_labels: int):
-        super().__init__()
-        self.bert = AutoModel.from_pretrained(model_name)
-        self.dropout = nn.Dropout(0.1)
-        self.linear = nn.Linear(self.bert.config.hidden_size, num_labels)
-        self.crf = CRF(num_labels, batch_first=True)
+	def __init__(self, model_name: str, num_labels: int):
+		super().__init__()
+		self.bert = AutoModel.from_pretrained(model_name)
+		self.dropout = nn.Dropout(0.1)
+		self.linear = nn.Linear(self.bert.config.hidden_size, num_labels)
+		self.crf = CRF(num_labels, batch_first=True)
 
-    def forward(self, input_ids, attention_mask, labels=None):
-        outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
-        sequence_output = self.dropout(outputs.last_hidden_state)
-        emissions = self.linear(sequence_output)
+	def forward(self, input_ids, attention_mask, labels=None):
+		outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+		sequence_output = self.dropout(outputs.last_hidden_state)
+		emissions = self.linear(sequence_output)
 
-        mask = attention_mask.bool()
+		mask = attention_mask.bool()
 
-        if labels is not None:
-            # Set the padding labels to 0 as -100 is out of range for CRF
-            labels = labels.clone()
-            labels[~mask] = 0
+		if labels is not None:
+			labels = labels.clone()
+			mask = labels != -100
+			labels[~mask] = 0
 
-            loss = -self.crf(emissions, labels, mask=attention_mask.bool(), reduction='mean')
-            return {"loss": loss, "logits:": emissions}
-        else:
-            predictions = self.crf.decode(emissions, mask=attention_mask.bool())
-            return {"logits": predictions}
+			loss = -self.crf(emissions, labels, mask=mask, reduction="mean")
+			return {"loss": loss, "logits": emissions}
+		else:
+			mask = attention_mask.bool()
+			predictions = self.crf.decode(emissions, mask=mask)
+			return {"logits": predictions}
