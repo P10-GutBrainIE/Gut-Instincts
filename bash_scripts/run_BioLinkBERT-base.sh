7e7db209b6d729427ce16afed8cb83ede7de035e
lasse2507a <lran18@student.aau.dk>
2025-04-23 14:20:06 +0200

    bash

--- bash_scripts/run_BioLinkBERT-base.sh
+++ bash_scripts/run_BioLinkBERT-base.sh
@@ -8,4 +8,8 @@
 
 mkdir -p logs
 
+# Preprocessing
+srun singularity exec /ceph/container/python/python_3.12.sif python src/preprocessing/main.py --config training_configs/BioLinkBERT-base.yaml
+
+# Training
 srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/BioLinkBERT-base.yaml > logs/BioLinkBERT-base.out 2> logs/BioLinkBERT-base.err
