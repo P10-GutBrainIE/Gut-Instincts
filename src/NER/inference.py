7e44dcd05122e4cfeb94a93c892f5de0f6be9b0b
Dolmer1 <72791404+Dolmer1@users.noreply.github.com>
2025-04-24 15:29:25 +0200

    albert debugging

--- src/NER/inference.py
+++ src/NER/inference.py
@@ -93,6 +93,8 @@ class NERInference:
 	def _ner_pipeline(self, text):
 		result = self.tokenizer(
 			text,
+			padding="max_length",
+			truncation=True,
 			return_tensors="pt",
 			return_offsets_mapping=True,
 		)
