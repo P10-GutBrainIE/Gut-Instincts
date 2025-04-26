2ddc15f122a29a7ac657d55d8300c04b8d7eff3c
lasse2507a <lran18@student.aau.dk>
2025-04-26 13:10:42 +0200

    bash lr test

--- bash_scripts/run_lr_test.sh
+++ bash_scripts/run_lr_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=LR_finder_test
-#SBATCH --output=LR_finder_test.out
-#SBATCH --error=LR_finder_test.err
+#SBATCH --job-name=lr_test
+#SBATCH --output=lr_test.out
+#SBATCH --error=lr_test.err
 #SBATCH --mem=24G
 #SBATCH --cpus-per-task=15
 #SBATCH --gres=gpu:1
@@ -9,7 +9,7 @@
 mkdir -p logs
 
 # preprocessing
-srun singularity exec /ceph/container/python/python_3.12.sif python src/preprocessing/main.py --config training_configs/pretrained/LR_finder_test.yaml
+srun singularity exec /ceph/container/python/python_3.12.sif python src/preprocessing/main.py --config training_configs/lr_test.yaml
 
 # training
-srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/pretrained/LR_finder_test.yaml >logs/LR_finder_test.out 2>logs/LR_finder_test.err
+srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config training_configs/lr_test.yaml >logs/lr_test.out 2>logs/lr_test.err
