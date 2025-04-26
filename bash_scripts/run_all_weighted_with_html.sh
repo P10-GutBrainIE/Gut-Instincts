1bdb18d228c2cee3bb715d3830f45c1a815c490c
lasse2507a <lran18@student.aau.dk>
2025-04-26 23:00:55 +0200

    all weighted with html

---
+++ bash_scripts/run_all_weighted_with_html.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+mkdir -p logs
+
+for script in training_configs/weighted_training_with_html/*.yaml; do
+    filename=$(basename "$script")
+    echo "Submitting $script"
+
+    sbatch <<EOF
+#!/bin/bash
+#SBATCH --job-name=$filename
+#SBATCH --output=$filename.out
+#SBATCH --error=$filename.err
+#SBATCH --mem=24G
+#SBATCH --cpus-per-task=15
+#SBATCH --gres=gpu:1
+
+# Preprocessing step
+echo "Running preprocessing for $script"
+srun singularity exec /ceph/container/python/python_3.12.sif python src/preprocessing/main.py --config $script
+
+# Training step
+echo "Running training for $script"
+srun singularity exec --nv /ceph/container/python/python_3.12.sif python src/NER/training.py --config $script >logs/$filename.out 2>logs/$filename.err
+EOF
+
+done
