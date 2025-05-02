10bfb4022cdaa7a68046e123d7bbf901ecff7667
lasse2507a <lran18@student.aau.dk>
2025-05-02 17:25:47 +0200

    mlflow

--- src/training/mlflow_results.py
+++ src/training/mlflow_results.py
@@ -32,16 +32,21 @@ def print_top_n_experiments(experiment_name: str = None, top_n: int = 20):
 		qualities = run.data.params.get("dataset_qualities", "Not logged")
 		remove_html = run.data.params.get("remove_html", "Not logged")
 		model_name = run.data.params.get("model_name", "Not logged").split("/")[-1]
-		results.append((model_name, qualities, weights, remove_html, best_f1))
+		experiment_name = run.data.params.get("experiment_name", "Not logged")
+		results.append((experiment_name, model_name, qualities, weights, remove_html, best_f1))
 
-	results.sort(key=lambda x: x[4], reverse=True)
+	results.sort(key=lambda x: x[5], reverse=True)
 
 	print(f"Best F1_micro values for {experiments_text}:")
 	print(f"{'No.':<5} {'Model Name':<52} {'Qualities':<42} {'Weights':<26} {'Remove HTML':<12} {'F1_micro':<8}")
-	print("-" * 152)
-	for i, (model_name, qualities, weights, remove_html, best_f1) in enumerate(results[:top_n], start=1):
-		print(f"{i:<5} {model_name:<52} {qualities:<42} {weights:<26} {remove_html:<12} {best_f1:<8.4f}")
+	print("-" * 182)
+	for i, (experiment_name, model_name, qualities, weights, remove_html, best_f1) in enumerate(
+		results[:top_n], start=1
+	):
+		print(
+			f"{i:<5} {experiment_name:<52} {model_name:<52} {qualities:<42} {weights:<26} {remove_html:<12} {best_f1:<8.4f}"
+		)
 
 
 if __name__ == "__main__":
-	print_top_n_experiments(experiment_name=None, top_n=20)
+	print_top_n_experiments(experiment_name=None, top_n=50)
