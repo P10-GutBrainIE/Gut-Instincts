0401575e03040aa741a901c7099c7365431a25ce
lasse2507a <lran18@student.aau.dk>
2025-05-07 14:53:25 +0200

    dataset

--- src/training/dataset.py
+++ src/training/dataset.py
@@ -6,12 +6,6 @@ class Dataset(torch.utils.data.Dataset):
 		self.data = data
 		self.with_weights = with_weights
 
-		first_label = self.data[0]["labels"]
-		if isinstance(first_label, (list, tuple)):
-			self.task_type = "sequence"
-		else:
-			self.task_type = "classification"
-
 	def __len__(self):
 		return len(self.data)
 
@@ -19,11 +13,7 @@ class Dataset(torch.utils.data.Dataset):
 		sample = self.data[idx]
 		sample["input_ids"] = torch.as_tensor(sample["input_ids"], dtype=torch.long)
 		sample["attention_mask"] = torch.as_tensor(sample["attention_mask"], dtype=torch.long)
-
-		if self.task_type == "sequence":
-			sample["labels"] = torch.as_tensor(sample["labels"], dtype=torch.long)
-		elif self.task_type == "classification":
-			sample["labels"] = torch.as_tensor(sample["labels"], dtype=torch.long).squeeze()
+		sample["labels"] = torch.as_tensor(sample["labels"], dtype=torch.long)
 
 		if self.with_weights:
 			sample["weight"] = torch.as_tensor(sample["weight"], dtype=torch.float)
