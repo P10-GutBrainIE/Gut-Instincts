bf3682abf8eac3a8e174df9faa6f53719b22dd7c
lasse2507a <lran18@student.aau.dk>
2025-05-09 15:46:53 +0200

    training configs

--- src/training/mlflow_results.py
+++ src/training/mlflow_results.py
@@ -46,27 +46,38 @@ def print_top_n_experiments(experiment_name: str = None, top_n: int = 20, task="
 			else:
 				best_f1 = float("-inf")
 
-			weights = run.get("params.dataset_weights", "No weights")
-			qualities = run.get("params.dataset_qualities", "Not logged")
-			qualities = str(re.findall("[pgsb]", qualities))
-			model_name = run.get("params.model_name", "Not logged").split("/")[-1]
-			experiment_name = run.get("params.experiment_name", "Not logged")
+			weights = run.get("params.dataset_weights") if run.get("params.dataset_weights") else "No weights"
+			qualities = run.get("params.dataset_qualities") if run.get("params.dataset_qualities") else "Not logged"
+			qualities = str(re.findall(r"(de|[pgsb])", qualities))
+			model_name = run.get("params.model_name") if run.get("params.model_name") else "Not logged"
+			model_name = model_name.split("/")[-1]
+			experiment_name = run.get("params.experiment_name") if run.get("params.experiment_name") else "Not logged"
+			seed = run.get("params.seed") if run.get("params.seed") else "No"
 			if task == "re":
-				multiplier = run.get("params.negative_sample_multiplier", "Not logged")
-				results.append((experiment_name, model_name, f"{qualities} x {multiplier}", weights, best_f1))
+				multiplier = (
+					run.get("params.negative_sample_multiplier")
+					if run.get("params.negative_sample_multiplier")
+					else "Not logged"
+				)
+
+				results.append((experiment_name, model_name, f"{qualities} x {multiplier}", weights, seed, best_f1))
 			else:
-				results.append((experiment_name, model_name, qualities, weights, best_f1))
+				results.append((experiment_name, model_name, qualities, weights, seed, best_f1))
 		except Exception as e:
 			print(f"Error processing run {run_id}: {e}")
 
-	results.sort(key=lambda x: x[4], reverse=True)
+	results.sort(key=lambda x: x[5], reverse=True)
 
 	print(f"Best F1_micro values for {experiments_text}:")
-	print(f"{'No.':<5} {'Experiment Name':<42} {'Model Name':<54} {'Qualities':<23} {'Weights':<24} {'F1_micro':<8}")
-	print("-" * 160)
-	for i, (experiment_name, model_name, qualities, weights, best_f1) in enumerate(results[:top_n], start=1):
-		print(f"{i:<5} {experiment_name:<42} {model_name:<54} {qualities:<23} {str(weights):<24} {best_f1:<8.4f}")
+	print(
+		f"{'No.':<5} {'Experiment Name':<42} {'Model Name':<54} {'Qualities':<34} {'Weights':<30} {'Seed':<6} {'F1_micro':<8}"
+	)
+	print("-" * 180)
+	for i, (experiment_name, model_name, qualities, weights, seed, best_f1) in enumerate(results[:top_n], start=1):
+		print(
+			f"{i:<5} {experiment_name:<42} {model_name:<54} {qualities:<34} {str(weights):<30} {seed:<6} {best_f1:<8.4f}"
+		)
 
 
 if __name__ == "__main__":
-	print_top_n_experiments(experiment_name=None, top_n=50, task="ner")
+	print_top_n_experiments(experiment_name=None, top_n=50, task="re")
