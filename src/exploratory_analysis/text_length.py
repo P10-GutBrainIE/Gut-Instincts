8feb54d357b5c12443e00dfc881ec6b503d83de2
lasse2507a <lran18@student.aau.dk>
2025-05-15 15:54:09 +0200

    token sequence length

--- src/exploratory_analysis/text_length.py
+++ src/exploratory_analysis/text_length.py
@@ -5,63 +5,78 @@ from matplotlib.patches import Patch
 import numpy as np
 import seaborn as sns
 import pandas as pd
+from transformers import AutoTokenizer
 
 
 def text_length_histogram(
 	file_paths: str, save_path: str = os.path.join("plots", "exploratory_analysis", "text_length_histogram.pdf")
 ):
-	text_lengths = {quality: {"title": [], "abstract": []} for quality in file_paths.keys()}
+	text_lengths = {quality: [] for quality in file_paths.keys()}
+
+	tokenizer = AutoTokenizer.from_pretrained("michiyasunaga/BioLinkBERT-base", use_fast=True)
+
+	exclude_cnt = 0
 
 	for quality, file_path in file_paths.items():
 		with open(file_path, "r", encoding="utf-8") as f:
 			file_data = json.load(f)
 
-			for _, content in file_data.items():
-				text_lengths[quality]["title"].append(len(content["metadata"]["title"].split(" ")))
-				text_lengths[quality]["abstract"].append(len(content["metadata"]["abstract"].split(" ")))
+			for paper_id, content in file_data.items():
+				length = sum(
+					tokenizer(
+						content["metadata"]["title"],
+						truncation=False,
+					)["attention_mask"]
+				) + sum(
+					tokenizer(
+						content["metadata"]["abstract"],
+						truncation=False,
+					)["attention_mask"]
+				)
+				if length > 1000:
+					print(f"Exluding {paper_id} from {quality} quality. Token count {length} > 1000.")
+					exclude_cnt += 1
+				else:
+					text_lengths[quality].append(length)
 
 	data = []
 	for quality, lengths in text_lengths.items():
-		for text_type in ["title", "abstract"]:
-			for length in lengths[text_type]:
-				data.append({"quality": quality, "text type": text_type, "length": length})
+		for length in lengths:
+			data.append({"quality": quality, "length": length})
 	df = pd.DataFrame(data)
 
-	df = df[df["length"] <= 900]
+	print(f"{len(df[df['length'] >= 512]) + exclude_cnt} out of {len(df) + exclude_cnt} with length > 512")
 
 	sns.set_theme(style="ticks")
-	fig, axes = plt.subplots(2, 4, figsize=(14, 7))
+	_, axes = plt.subplots(1, 4, figsize=(14, 5))
 
 	palette = sns.color_palette("magma", n_colors=4, desat=1)[::-1]
 
 	qualities = ["platinum", "gold", "silver", "bronze"]
 
-	title_data = df[df["text type"] == "title"]["length"]
-	abstract_data = df[df["text type"] == "abstract"]["length"]
-
-	title_xlim = (title_data.min(), title_data.max())
-	abstract_xlim = (abstract_data.min(), abstract_data.max())
-
-	title_data = df[df["text type"] == "title"]["length"]
 	for i, quality in enumerate(qualities):
 		sns.histplot(
-			data=df[(df["text type"] == "title") & (df["quality"] == quality)],
+			data=df[df["quality"] == quality],
 			x="length",
 			hue="quality",
 			palette=palette[i : i + 1],
 			multiple="stack",
-			ax=axes[0, i],
+			ax=axes[i],
 			bins=25,
 			legend=False,
 			alpha=1,
 			edgecolor="black",
 			linewidth=0.5,
 		)
-		axes[0, i].set_xlabel("")
-		axes[0, i].set_ylabel("")
-		axes[0, i].set_xlim(title_xlim)
-		axes[0, i].set_xticks(np.arange(0, title_data.max(), step=10))
-	fig.text(0.46, 0.985, "Titles", ha="center", va="center", fontsize=16)
+		axes[i].set_xlabel("")
+		axes[i].set_ylabel("")
+
+		lengths_data = df["length"]
+		x_min, x_max = lengths_data.min(), lengths_data.max()
+		axes[i].set_xlim((x_min, x_max))
+		axes[i].set_xticks(np.arange(0, x_max, step=200))
+		if i == 0:
+			axes[i].set_ylabel("Frequency", fontsize=14)
 
 	unique_qualities = df["quality"].unique()
 	handles = [
@@ -69,35 +84,9 @@ def text_length_histogram(
 		for i, quality in enumerate(unique_qualities)
 	]
 	handles.reverse()
-	axes[0, 3].legend(
-		handles=handles, title="Quality", bbox_to_anchor=(1, 1), loc="upper left", fontsize=12, title_fontsize=14
-	)
+	plt.legend(title="Quality", handles=handles, fontsize=12, title_fontsize=14)
 
-	title_data = df[df["text type"] == "abstract"]["length"]
-	for i, quality in enumerate(qualities):
-		sns.histplot(
-			data=df[(df["text type"] == "abstract") & (df["quality"] == quality)],
-			x="length",
-			hue="quality",
-			palette=palette[i : i + 1],
-			multiple="stack",
-			ax=axes[1, i],
-			bins=20,
-			legend=False,
-			alpha=1,
-			edgecolor="black",
-			linewidth=0.5,
-		)
-		axes[1, i].set_xlabel("")
-		axes[1, i].set_ylabel("")
-		axes[1, i].set_xlim(abstract_xlim)
-		axes[1, i].set_xticks(np.arange(0, title_data.max(), step=100))
-	plt.subplots_adjust(hspace=5)
-	fig.text(0.46, 0.485, "Abstracts", ha="center", va="center", fontsize=16)
-
-	plt.figtext(0.46, 0.0, "Word Count", ha="center", fontsize=14)
-	plt.figtext(0.006, 0.46, "Frequency", ha="center", rotation=90, fontsize=14)
-
+	plt.figtext(0.515, 0.0, "Token Count", ha="center", fontsize=14)
 	sns.despine()
 	plt.tight_layout(pad=1.3)
 	os.makedirs(os.path.dirname(save_path), exist_ok=True)
